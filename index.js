abddceb9a370bfba938a960b123ff5d4e425a16c
Yash <yashbhardwaj0307@gmail.com>
2025-08-13 02:52:41 +0530

    added instructor middleware and courses end points

--- index.js
+++ index.js
@@ -3,21 +3,22 @@ import {Client} from 'pg';
 import dotenv from "dotenv";
 import jwt  from 'jsonwebtoken';
 import bcrypt from "bcrypt";
-import { UserAuth } from './middleware.js';
+import { UserAuth,InstructorAuth } from './middleware.js';
 dotenv.config();
 const app=express();
 app.use(express.json());
 const pgClient = new Client({connectionString:process.env.BACKEND_URI})
 app.post("/user/signup",async function(req,res){
-    const { full_name, password, email } = req.body;
+    const { full_name, password, email,role } = req.body;
     const password_hash=await bcrypt.hash(password,10);
     try{await pgClient.query(
-          `INSERT INTO Users (full_name, password_hash, email) VALUES ($1, $2, $3)`,[full_name, password_hash, email]);
-            res.json({
+          `INSERT INTO Users (full_name, password_hash, email,role) VALUES ($1, $2, $3,$4)`,[full_name, password_hash, email,role]);
+            res.status(201).json({
             message:"signed up"
         })}
     catch(e){
-        console.error("Failed to sign in error:",e);
+        console.error("Failed to sign up error:",e);
+        res.status(500).json({ message: "Signup failed" });
     }
 })
 app.post("/user/signin", async function (req, res) {
@@ -33,25 +34,66 @@ app.post("/user/signin", async function (req, res) {
         if (!isMatch) {
             return res.status(401).json({ message: "Wrong credentials" });
         }
-        const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET);
+        const token = jwt.sign({ user_id: user.user_id,
+        email: user.email,
+        role: user.role,
+        full_name: user.full_name}, process.env.JWT_SECRET,{ expiresIn: "2d" });
         res.json({
             message: "Signed in successfully",
             token: token
         });
-    } catch (error) {
+        } catch (error) {
         console.error("Sign-in error:", error);
         res.status(500).json({ message: "Internal server error" });
-    }
+        }
 });
 app.get("/user/purchases",UserAuth,function(req,res){
     res.json({
         message:"my purchases end point"
     })
 })
-app.get("/courses",UserAuth,function(req,res){
+app.get("/courses",UserAuth,async function(req,res){
+    try{const data=await pgClient.query("SELECT * FROM courses ORDER BY created_at DESC");
+    const courselist=data.rows;
     res.json({
-        message:"available courses end point"
+        message:"available courses end point",
+        courses:courselist
     })
+    }
+    catch(e){
+        console.error("error occured while fetching courses :",e);
+        res.status(500).json({
+            message: "Failed to fetch courses",
+            error: e.message
+        });
+    }
+})
+app.get("/courses/:id",UserAuth,async function(req,res){
+    try{const data=await pgClient.query("SELECT * FROM courses where course_id=$1",[req.params.id]);
+    const courselist=data.rows;
+    res.json({
+        message:"available courses end point",
+        courses:courselist
+    })
+    }
+    catch(e){
+        console.error("error occured while fetching courses :",e);
+        res.status(500).json({
+            message: "Failed to fetch courses",
+            error: e.message
+        });
+    }
+})
+app.post("/courses",InstructorAuth,async function(req,res){
+    try{
+    const {title,price}=req.body;
+    await pgClient.query("INSERT INTO courses (title, price, instructor_id) VALUES ($1, $2, $3)",[title, price, req.user_id]);
+    res.json({
+        message:"course added"
+    })}catch(e){
+        console.error("Error occured while adding course :",e);
+        res.status(500).json({ error: "Internal server error" });
+    }
 })
 app.post("/course/purchase",UserAuth,function(req,res){
     res.json({
--- middleware.js
+++ middleware.js
@@ -6,11 +6,21 @@ export const UserAuth=(req,res,next)=>{
         return res.status(401).json({ message: "token is missing" });
     }
     const decodedToken=jwt.verify(token,process.env.JWT_SECRET);
-    req.email=decodedToken.email
+    req.email=decodedToken.email;
+    req.role=decodedToken.role;
+    req.user_id=decodedToken.user_id;
     next();
     }
     catch(e){
         console.error("invalid token",e);
         return res.status(401).json({ message: "Invalid or expired token" });
     }
+}
+export const InstructorAuth=(req,res,next)=>{
+    UserAuth(req, res, () => {
+        if (req.role === "instructor") {
+            return next();
+        }
+        return res.status(403).json({ message: "Access denied: not an instructor" });
+    });
 }
