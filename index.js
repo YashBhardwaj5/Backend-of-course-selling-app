89a328fb39d83bd247504c984a8a8554b363ecfd
Yash <yashbhardwaj0307@gmail.com>
2025-08-13 21:20:57 +0530

    added courses and lessons end points

--- index.js
+++ index.js
@@ -84,6 +84,40 @@ app.get("/courses/:id",UserAuth,async function(req,res){
         });
     }
 })
+app.put("/courses/:id",InstructorAuth,async function(req,res){
+    try{
+        const courseCheck = await pgClient.query("SELECT * FROM courses WHERE course_id = $1 AND instructor_id = $2",[req.params.id, req.user_id]);
+        if (courseCheck.rowCount === 0) {
+            return res.status(403).json({ message: "Not authorized for this course" });
+        }
+        await pgClient.query("UPDATE courses SET title = COALESCE($1, title),price = COALESCE($2, price),description = COALESCE($3, description),thumbnail_url = COALESCE($4,thumbnail_url) WHERE course_id = $5 AND instructor_id = $6",[req.body.title||null,req.body.price|| null,req.body.description||null,req.body.thumbnail_url||null,req.params.id,req.user_id]);
+        res.json({
+        message:"Course updated successfully"
+    })
+    }
+    catch(e){
+        console.error("Error occurred while updating courses:", e);
+            res.status(500).json({
+            message: "Failed to update courses",
+            error: e.message
+        });
+    }
+})
+
+app.delete("/courses/:id",InstructorAuth,async function(req,res){
+    try{
+        const courseCheck = await pgClient.query("SELECT * FROM courses WHERE course_id = $1 AND instructor_id = $2",[req.params.id, req.user_id]);
+        if (courseCheck.rowCount === 0) {
+            return res.status(403).json({ message: "Not authorized for this course" });
+        }
+        await pgClient.query("DELETE FROM courses where course_id=$1",[req.params.id]);
+        res.json({
+            message:"Course deleted"
+        })}catch(e){
+            console.error("Error occured while deleting a course :",e);
+            res.status(500).json({ error: "Internal server error" });
+        }
+})
 app.post("/courses",InstructorAuth,async function(req,res){
     try{
     const {title,price}=req.body;
@@ -93,6 +127,114 @@ app.post("/courses",InstructorAuth,async function(req,res){
     })}catch(e){
         console.error("Error occured while adding course :",e);
         res.status(500).json({ error: "Internal server error" });
+    }
+})
+app.get("/courses/:courseId/lessons",UserAuth,async function(req,res){
+    try{
+    const data=await pgClient.query("SELECT * FROM lessons WHERE course_id=$1 ORDER BY position",[req.params.courseId]); 
+    const lessons=data.rows;
+    res.json({
+        message:"Lessons fetched successfully",
+        lessons:lessons
+    })
+    }catch(e){
+        console.error("Error occurred while fetching lessons",e);
+        res.status(500).json({
+            message:"Error occurred while fetching lessons",
+            error:e.message
+        })
+    }
+})
+app.get("/lessons/:lessonid",UserAuth,async function(req,res){
+    try{
+    const data=await pgClient.query("SELECT * FROM lessons WHERE lesson_id=$1",[req.params.lessonid]); 
+    
+    if (data.rowCount === 0) {
+    return res.status(404).json({ message: "Lesson not found" });
+    }
+    const lesson=data.rows[0];
+    res.json({
+        message:"Lessons fetched successfully",
+        lesson:lesson
+    })
+    }catch(e){
+        console.error("Error occurred while fetching lessons",e);
+        res.status(500).json({
+            message:"Error occurred while fetching lessons",
+            error:e.message
+        })
+    }
+})
+app.post("/courses/:courseId/lessons",InstructorAuth,async function(req,res){
+    
+    try{
+        const {title,video_url,content,position}=req.body;
+        const courseCheck = await pgClient.query("SELECT * FROM courses WHERE course_id = $1 AND instructor_id = $2",[req.params.courseId, req.user_id]);
+        if (courseCheck.rowCount === 0) {
+            return res.status(403).json({ message: "Not authorized for this course" });
+        }
+        await pgClient.query("INSERT INTO lessons (course_id,title,video_url,content,position) VALUES ($1,$2,$3,$4,$5)",[req.params.courseId,title,video_url,content,position]);
+        res.status(201).json({
+            message:"Lesson added successfully"
+        })}
+        catch(e){
+            res.status(500).json({
+            message:"Error occurred while adding lessons",
+            error:e.message
+        })
+    }
+})
+
+app.put("/lessons/:lessonid",InstructorAuth,async function(req,res){
+    try{
+        const { title, content, video_url, position } = req.body;
+        const check = await pgClient.query(
+            `SELECT lessons.lesson_id
+             FROM lessons
+             JOIN courses ON lessons.course_id = courses.course_id
+             WHERE lessons.lesson_id = $1 AND courses.instructor_id = $2`,
+            [req.params.lessonid, req.user_id]
+        );
+        if (check.rowCount === 0) {
+            return res.status(403).json({ message: "Not authorized to update this lesson" });
+        }
+        await pgClient.query(
+                `UPDATE lessons 
+                SET title = COALESCE($1, title),
+                content = COALESCE($2, content),
+                video_url = COALESCE($3, video_url),
+                position= COALESCE($4, position)
+                WHERE lesson_id = $5`,
+                [title || null, content || null, video_url || null,position||null, req.params.lessonid]
+        );
+        res.json({
+        message:"Lesson updated successfully"
+        })
+    }
+    catch(e){
+        console.error("Error occurred while updating lesson :",e);
+        res.status(500).json({ message: "Error occurred while updating lesson" });
+    }
+})
+app.delete("/lessons/:lessonid",InstructorAuth,async function(req,res){
+    try{
+        const check = await pgClient.query(
+            `SELECT lessons.lesson_id
+             FROM lessons
+             JOIN courses ON lessons.course_id = courses.course_id
+             WHERE lessons.lesson_id = $1 AND courses.instructor_id = $2`,
+            [req.params.lessonid, req.user_id]
+        );
+        if (check.rowCount === 0) {
+            return res.status(403).json({ message: "Not authorized to delete this lesson" });
+        }
+        await pgClient.query(`DELETE FROM lessons WHERE lesson_id=$1`,[req.params.lessonid]);
+        res.json({
+            message:"Lesson deleted successfully"
+        })}
+    catch(e){
+        console.error("Error occurred while deleting lesson :",e);
+        res.status(500).json({ message: "Error occurred while deleting lesson" });
     }
 })
 app.post("/course/purchase",UserAuth,function(req,res){
@@ -109,7 +251,7 @@ const startServer = async () => {
         });
     } catch (err) {
         console.error("Failed to connect to DB:", err);
-        process.exit(1); // stop the app
+        process.exit(1); 
     }
 };
 startServer();
